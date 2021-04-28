4ce5a769bf92a52907c5c82d80f2c5c2b2081fed
mn-a <83325423+mn-a@users.noreply.github.com>
2021-04-28 21:20:10 +0900

    GitHub for git pull

--- hello.py
+++ hello.py
@@ -1,7 +1,9 @@
 // for command line git tutorial
-// GitHub modification
+// First: Local repository modification
+// Second: GitHub modification
 
 print("Hello World")
 print("Tell your world")
 print("Tell his world")
 print("Tell her world")
+print("Tell my world")
